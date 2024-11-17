5b12c825b4b3b709a2678e516aa93dfe8b5a694c
arhamfareed106 <arhamfareed575@gmail.com>
2024-11-17 22:21:28 +0500

    roon djano

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -80,6 +80,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'djangochat.wsgi.application'
 ASGI_APPLICATION = 'djangochat.asgi.application'
 
+CHANNEL_LAYERS ={
+    'default':{
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
+
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
